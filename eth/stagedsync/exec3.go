ee5d3beb6654548ba4a37c7f2037fdaea36a2ff7
alex.sharov <AskAlexSharov@gmail.com>
2024-11-19 12:24:30 +0700

    save

--- eth/stagedsync/exec3.go
+++ eth/stagedsync/exec3.go
@@ -313,7 +313,7 @@ func ExecV3(ctx context.Context,
 	if err != nil {
 		return err
 	}
-	viewID, _ := applyTx.ViewID()
+	viewID := applyTx.ViewID()
 	log.Info(fmt.Sprintf("[%s] starting", execStage.LogPrefix()),
 		"inMem", inMemExec, "from", blockNum, "to", maxBlockNum, "execStage.BlockNumber", execStage.BlockNumber, "maxBlockNum", maxBlockNum, "prevStageProgress", _prevStageProgress, "viewID", viewID, "fromTxNum", doms.TxNum(), "offsetFromBlockBeginning", offsetFromBlockBeginning, "initialCycle", initialCycle, "useExternalTx", useExternalTx, "inMem", inMemExec)
 	//}

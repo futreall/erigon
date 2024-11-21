c313ad217c5f40f56c56c0b414478e8b8f2a8d30
Somnath Banerjee <snb895@outlook.com>
2024-11-21 15:37:13 +0400

    Revert aura json changes

--- core/genesis_write.go
+++ core/genesis_write.go
@@ -439,7 +439,8 @@ func GnosisGenesisBlock() *types.Genesis {
 	return &types.Genesis{
 		Config:     params.GnosisChainConfig,
 		Timestamp:  0,
-		AuRaSeal:   types.NewAuraSeal(0 , common.FromHex("0x0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000")),		GasLimit:   0x989680,
+		AuRaSeal:   common.FromHex("0x0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"),
+		GasLimit:   0x989680,
 		Difficulty: big.NewInt(0x20000),
 		Alloc:      readPrealloc("allocs/gnosis.json"),
 	}
@@ -448,7 +449,7 @@ func GnosisGenesisBlock() *types.Genesis {
 func GnoPectraDevnet1GenesisBlock() *types.Genesis {
 	return &types.Genesis{
 		Config:     params.GnoPectraDevnet1Config,
-		AuRaSeal:   types.NewAuraSeal(0 , common.FromHex("0x0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000")),
+		AuRaSeal:   common.FromHex("0x0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"),
 		GasLimit:   0x989680,
 		Difficulty: big.NewInt(1),
 		BaseFee:    big.NewInt(0x3b9aca00),
@@ -456,11 +457,12 @@ func GnoPectraDevnet1GenesisBlock() *types.Genesis {
 	}
 }
 
+
 func ChiadoGenesisBlock() *types.Genesis {
 	return &types.Genesis{
 		Config:     params.ChiadoChainConfig,
 		Timestamp:  0,
-		AuRaSeal:   types.NewAuraSeal(0 , common.FromHex("0x0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000")),
+		AuRaSeal:   common.FromHex("0x0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"),
 		GasLimit:   0x989680,
 		Difficulty: big.NewInt(0x20000),
 		Alloc:      readPrealloc("allocs/chiado.json"),
@@ -516,11 +518,9 @@ func GenesisToBlock(g *types.Genesis, tmpDir string, logger log.Logger) (*types.
 		BaseFee:       g.BaseFee,
 		BlobGasUsed:   g.BlobGasUsed,
 		ExcessBlobGas: g.ExcessBlobGas,
+		AuRaStep:      g.AuRaStep,
+		AuRaSeal:      g.AuRaSeal,
 		RequestsHash:  g.RequestsHash,
-	}
-	if g.AuRaSeal != nil && len(g.AuRaSeal.AuthorityRound.Signature) > 0{
-		head.AuRaSeal = g.AuRaSeal.AuthorityRound.Signature
-		head.AuRaStep = g.AuRaSeal.AuthorityRound.Step
 	}
 	if g.GasLimit == 0 {
 		head.GasLimit = params.GenesisGasLimit

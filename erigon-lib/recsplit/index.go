3a9ad4db5b1e239c467fce428df1de482212bd35
alex.sharov <AskAlexSharov@gmail.com>
2024-11-30 13:27:38 +0700

    save

--- erigon-lib/recsplit/index.go
+++ erigon-lib/recsplit/index.go
@@ -358,7 +358,7 @@ func (idx *Index) Lookup(bucketHash, fingerprint uint64) (uint64, bool) {
 	rec := int(cumKeys) + int(remap16(remix(fingerprint+idx.startSeed[level]+b), m))
 	pos := 1 + 8 + idx.bytesPerRec*(rec+1)
 
-	idx.touch(pos, 8)
+	idx.touch(pos, 7)
 	fmt.Printf("[dbg] %s, %d\n", idx.fileName, pos)
 	found := binary.BigEndian.Uint64(idx.data[pos:]) & idx.recMask
 	if idx.lessFalsePositives {
